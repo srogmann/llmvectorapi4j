1eca7d76726d41d5b72516995145752649c0b598
rogmann <github@rogmann.org>
2025-07-08 22:26:36 +0200

    JavaDoc (no OAuth support)

--- src/main/java/org/rogmann/llmva4j/mcp/package-info.java
+++ src/main/java/org/rogmann/llmva4j/mcp/package-info.java
@@ -2,5 +2,7 @@
  * This package contains classes to implement a simple MCP-client and/or MCP-server.
  * 
  * <p>See: <a href="https://modelcontextprotocol.io/specification/">MCP specification</a></p>
+ * 
+ * <p><strong>The OAuth-support required in version 2025-06-18 of the MCP is missing in this implementation!</strong></p>
  */
 package org.rogmann.llmva4j.mcp;
